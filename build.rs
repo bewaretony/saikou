41a2a73ca7f16e7ef87bfdd697f7896b6164bd69
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-05-04 18:26:40 -0600

    Initial commit

---
+++ build.rs
@@ -0,0 +1,56 @@
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
+
+use futures::future::Future;
+use futures::stream::Stream;
+
+use std::io::Write;
+
+fn fetch<C: hyper::client::Connect>(client: &hyper::Client<C>, url: &str) -> Box<Future<Item=Vec<String>,Error=()>> {
+    let url: hyper::Uri = url.parse().unwrap();
+    Box::new(client.get(url.clone()).map(move |res| {
+        if res.status() != hyper::StatusCode::Ok {
+            panic!("Failed to load {}", url);
+        }
+        res.body().concat2()
+    })
+    .map(|body| {
+        vec!["test".to_owned()]
+    }).map_err(|e| panic!("{:?}", e)))
+}
+
+fn main() {
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let handle = core.handle();
+
+    let client = hyper::Client::configure()
+        .connector(hyper_tls::HttpsConnector::new(2, &handle).unwrap())
+        .build(&handle);
+
+    let task = fetch(&client, "https://zh.moegirl.org/Template:ACG%E7%BB%8F%E5%85%B8%E5%8F%B0%E8%AF%8D")
+        .join3(
+            fetch(&client, "https://zh.moegirl.org/Template:ACG%E5%9C%88%E7%94%A8%E8%AF%AD"),
+            fetch(&client, "https://zh.moegirl.org/Template:%E5%A4%A9%E6%9C%9D%E7%BD%91%E7%BB%9C%E6%B5%81%E8%A1%8C%E8%AF%AD%E5%8F%A5"))
+        .and_then(|(a, b, c)| {
+            let result = a.into_iter()
+                .chain(b.into_iter())
+                .chain(c.into_iter());
+            let content = result.map(|x| format!("r#\"{}\"#", x))
+                .collect::<Vec<_>>()
+                .join(",\n        ");
+            let content = format!("lazy_static! {{
+    pub static ref MEMES: Vec<&'static str> = {{
+        {}
+    }}
+}}", content);
+            let out_dir = std::env::var("OUT_DIR").unwrap();
+            let path = std::path::Path::new(&out_dir).join("memes.rs");
+            let mut file = std::fs::File::create(&path).unwrap();
+            file.write_all(content.as_bytes()).unwrap();
+            Ok(())
+        });
+
+    core.run(task).unwrap();
+}
